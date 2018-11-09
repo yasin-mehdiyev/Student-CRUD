12a8fddbbe2fbdc738f09d047958521fe166f7cf
Yasin <Yasin@DESKTOP-JR5VDO0>
2018-11-09 09:04:03 +0400

    thecodding

---
+++ 07_11_2018/07_11_2018/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace _07_11_2018
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtCap = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtpBirthday = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdbMarried = new System.Windows.Forms.RadioButton();
+            this.rdbSingle = new System.Windows.Forms.RadioButton();
+            this.dgwStudents = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(70, 595);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(144, 64);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Əlavə et";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click_1);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(43, 43);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(220, 38);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(43, 113);
+            this.txtSurname.Multiline = true;
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(220, 38);
+            this.txtSurname.TabIndex = 1;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(43, 205);
+            this.txtPhone.Multiline = true;
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(220, 38);
+            this.txtPhone.TabIndex = 1;
+            // 
+            // txtCap
+            // 
+            this.txtCap.Location = new System.Drawing.Point(43, 530);
+            this.txtCap.Multiline = true;
+            this.txtCap.Name = "txtCap";
+            this.txtCap.Size = new System.Drawing.Size(220, 38);
+            this.txtCap.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ad";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(43, 84);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(74, 26);
+            this.label.TabIndex = 2;
+            this.label.Text = "Soyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(43, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telefon Nömrəsi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(43, 491);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 26);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "CAP";
+            // 
+            // dtpBirthday
+            // 
+            this.dtpBirthday.Checked = false;
+            this.dtpBirthday.CustomFormat = "dd.MM.yyyy";
+            this.dtpBirthday.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpBirthday.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpBirthday.Location = new System.Drawing.Point(43, 445);
+            this.dtpBirthday.Name = "dtpBirthday";
+            this.dtpBirthday.Size = new System.Drawing.Size(215, 32);
+            this.dtpBirthday.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 406);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Doğum günü";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdbMarried);
+            this.groupBox1.Controls.Add(this.rdbSingle);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(8, 260);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(298, 131);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ailə Vəziyyətiniz";
+            // 
+            // rdbMarried
+            // 
+            this.rdbMarried.AutoSize = true;
+            this.rdbMarried.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbMarried.Location = new System.Drawing.Point(157, 53);
+            this.rdbMarried.Name = "rdbMarried";
+            this.rdbMarried.Size = new System.Drawing.Size(73, 30);
+            this.rdbMarried.TabIndex = 0;
+            this.rdbMarried.Text = "Evli";
+            this.rdbMarried.UseVisualStyleBackColor = true;
+            // 
+            // rdbSingle
+            // 
+            this.rdbSingle.AutoSize = true;
+            this.rdbSingle.Checked = true;
+            this.rdbSingle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbSingle.Location = new System.Drawing.Point(10, 53);
+            this.rdbSingle.Name = "rdbSingle";
+            this.rdbSingle.Size = new System.Drawing.Size(99, 30);
+            this.rdbSingle.TabIndex = 0;
+            this.rdbSingle.TabStop = true;
+            this.rdbSingle.Text = "Subay";
+            this.rdbSingle.UseVisualStyleBackColor = true;
+            // 
+            // dgwStudents
+            // 
+            this.dgwStudents.AllowUserToAddRows = false;
+            this.dgwStudents.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgwStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwStudents.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dgwStudents.Location = new System.Drawing.Point(328, 12);
+            this.dgwStudents.Name = "dgwStudents";
+            this.dgwStudents.RowTemplate.Height = 28;
+            this.dgwStudents.Size = new System.Drawing.Size(996, 615);
+            this.dgwStudents.TabIndex = 5;
+            this.dgwStudents.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgwStudents_RowHeaderMouseClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Id";
+            this.Column1.Name = "Column1";
+            this.Column1.Visible = false;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Ad";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Soyad";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Telefon ";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Ailə Vəziyyəti";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Doğum Tarixi";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "CAP";
+            this.Column7.Name = "Column7";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(12, 595);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(144, 64);
+            this.btnUpdate.TabIndex = 0;
+            this.btnUpdate.Text = "Yenilə";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Visible = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(178, 595);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(144, 64);
+            this.btnDelete.TabIndex = 0;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Visible = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1366, 750);
+            this.Controls.Add(this.dgwStudents);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dtpBirthday);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCap);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtCap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtpBirthday;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdbMarried;
+        private System.Windows.Forms.RadioButton rdbSingle;
+        private System.Windows.Forms.DataGridView dgwStudents;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ 07_11_2018/07_11_2018/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Globalization;
+
+namespace _07_11_2018
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            getFullData();
+        }
+
+        private int selectedId;
+
+        //Select
+        private void getFullData()
+        {
+            dgwStudents.Rows.Clear();
+            string connectionString = "Server=DESKTOP-JR5VDO0; Database=Student;Integrated Security=SSPI";
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            string query = "SELECT * FROM Person";
+            SqlCommand command = new SqlCommand(query, con);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                dgwStudents.Rows.Add(reader.GetInt32(0),
+                                     reader.GetString(1),
+                                     reader.GetString(2),
+                                     reader.GetString(3),
+                                     reader.GetBoolean(5) ? "Evli" : "Subay",
+                                     reader.GetDateTime(4).ToString("dd.MM.yyyy"),
+                                     reader.GetDecimal(6).ToString("0.##")
+                                     );
+            }
+            con.Close();
+        }
+
+        //Insert
+        private void btnAdd_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtName.Text) || string.IsNullOrEmpty(txtSurname.Text) || string.IsNullOrEmpty(txtPhone.Text) || string.IsNullOrEmpty(txtCap.Text))
+            {
+                MessageBox.Show("Ad,Soyad,Telefon Nomrəsi və ya CAP-dan biri daxil edilməyib");
+                return;
+            }
+
+
+            if (!decimal.TryParse(txtCap.Text, out decimal CAP))
+            {
+                MessageBox.Show("CAP datasini duzgun daxil edin");
+                return;
+            }
+
+            string connectionString = "Server=DESKTOP-JR5VDO0; Database=Student;Integrated Security=SSPI";
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            string query = "INSERT INTO Person([Name],[Surname],[Phone],[Birthday],[IsMarried],[CAP]) VALUES(@name, @surname, @phone, @birthday, @ismarried, @cap); ";
+            SqlCommand command = new SqlCommand(query, con);
+            command.Parameters.AddWithValue("@name", txtName.Text);
+            command.Parameters.AddWithValue("@surname", txtSurname.Text);
+            command.Parameters.AddWithValue("@phone", txtPhone.Text);
+            command.Parameters.AddWithValue("@birthday", dtpBirthday.Value.ToString("yyyy-MM-dd"));
+            command.Parameters.AddWithValue("@ismarried", rdbSingle.Checked ? 0 : 1);
+            command.Parameters.AddWithValue("@cap", CAP);
+
+            int r = command.ExecuteNonQuery();
+            con.Close();
+            Reset();
+        }
+
+        private void Reset()
+        {
+            getFullData();
+            txtName.Text = "";
+            txtSurname.Text = "";
+            txtPhone.Text = "";
+            txtCap.Text = "";
+            rdbSingle.Checked = true;
+            dtpBirthday.Value = DateTime.Now;
+
+            btnDelete.Visible = false;
+            btnUpdate.Visible = false;
+            btnAdd.Visible = true;
+
+        }
+
+        private void dgwStudents_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            btnDelete.Visible = true;
+            btnUpdate.Visible = true;
+            btnAdd.Visible = false;
+
+            this.selectedId = Convert.ToInt32(dgwStudents.Rows[e.RowIndex].Cells[0].Value.ToString());
+            txtName.Text = dgwStudents.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtSurname.Text = dgwStudents.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtPhone.Text = dgwStudents.Rows[e.RowIndex].Cells[3].Value.ToString();
+            txtCap.Text = dgwStudents.Rows[e.RowIndex].Cells[6].Value.ToString();
+            dtpBirthday.Value = DateTime.ParseExact(dgwStudents.Rows[e.RowIndex].Cells[5].Value.ToString(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+            if (dgwStudents.Rows[e.RowIndex].Cells[4].Value.ToString()=="Evli")
+            {
+                rdbSingle.Checked = false;
+                rdbMarried.Checked = true;
+            }
+            else
+            {
+                rdbMarried.Checked = false;
+                rdbSingle.Checked = true;
+            }
+
+        }
+
+        //Delete
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("Silmək üçün əminsinizmi?", "Silmə işləmi", MessageBoxButtons.OKCancel);
+            if (r == DialogResult.OK)
+            {
+                string connectionString = "Server=DESKTOP-JR5VDO0; Database=Student;Integrated Security=SSPI";
+                SqlConnection con = new SqlConnection(connectionString);
+                con.Open();
+                string query = "DELETE Person WHERE ID=" + this.selectedId;
+                SqlCommand command = new SqlCommand(query, con);
+                command.ExecuteNonQuery();
+                con.Close();
+
+                Reset();
+            }
+        }
+
+        //Update
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtName.Text) || string.IsNullOrEmpty(txtSurname.Text) || string.IsNullOrEmpty(txtPhone.Text) || string.IsNullOrEmpty(txtCap.Text))
+            {
+                MessageBox.Show("Ad,Soyad,Telefon Nomrəsi və ya CAP-dan biri daxil edilməyib");
+                return;
+            }
+
+
+            if (!decimal.TryParse(txtCap.Text, out decimal CAP))
+            {
+                MessageBox.Show("CAP datasini duzgun daxil edin");
+                return;
+            }
+
+            string connectionString = "Server=DESKTOP-JR5VDO0; Database=Student;Integrated Security=SSPI";
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+
+
+            string query = "UPDATE Person SET [Name]=@name,[Surname]= @surname,[Phone]=@phone,[Birthday]=@birthday,[IsMarried]=@ismarried,[CAP]=@cap WHERE Id=@id; ";
+            SqlCommand command = new SqlCommand(query, con);
+            command.Parameters.AddWithValue("@name", txtName.Text);
+            command.Parameters.AddWithValue("@surname", txtSurname.Text);
+            command.Parameters.AddWithValue("@phone", txtPhone.Text);
+            command.Parameters.AddWithValue("@birthday", dtpBirthday.Value.ToString("yyyy-MM-dd"));
+            command.Parameters.AddWithValue("@ismarried", rdbSingle.Checked ? 0 : 1);
+            command.Parameters.AddWithValue("@cap", CAP);
+            command.Parameters.AddWithValue("@id", selectedId);
+
+            int r = command.ExecuteNonQuery();
+            con.Close();
+            Reset();
+        }
+
+    }
+}
